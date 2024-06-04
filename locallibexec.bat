134e762faffa52d26c0c35c94e961f92c2c06e7e
rodi38 <rodrigorochaed1@gmail.com>
2024-06-03 22:32:17 -0300

    script

---
+++ locallibexec.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+
+set "projectPath=%~dp0"
+
+cd /d "%projectPath%"
+
+if exist "node_modules" (
+    echo 'npm install' já foi executado anteriormente. Pulando para npm run dev...
+    echo Executando npm run dev...
+    start cmd /k "npm run dev"
+
+    echo Abrindo navegador em http://localhost:5173
+    start http://localhost:5173
+) else (
+    echo Executando npm install...
+    start cmd /k "npm install && start http://localhost:5173 && npm run dev "
+)
+
+
+exit
